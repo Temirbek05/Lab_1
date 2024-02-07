98ebf783f29f8c7498dd1ea075bf37284e7aa900
Temirbek05 <156643192+Temirbek05@users.noreply.github.com>
2024-02-07 11:34:55 +0600

    Add files via upload

---
+++ lab-2/sets/lists_00.py
@@ -0,0 +1,30 @@
+"""
+Lists are used to store multiple items in a single variable.
+
+Lists are one of 4 built-in data types in Python used to store collections of data, the other 3 are Tuple, Set, and Dictionary, all with different qualities and usage.
+
+Lists are created using square brackets:
+
+
+"""
+
+# to create a List 
+
+thislist = ["apple", "banana", "cherry"]
+print(thislist)
+
+# Using the list() constructor to make a List:
+
+thislist = ["apple", "banana", "cherry"]
+print(thislist)
+
+# String, int and boolean data types:
+
+list1 = ["apple", "banana", "cherry"]
+list2 = [1, 5, 7, 9, 3]
+list3 = [True, False, False]
+
+# To determine how many items a list has, use the len() function:
+
+thislist = ["apple", "banana", "cherry"]
+print(len(thislist))
